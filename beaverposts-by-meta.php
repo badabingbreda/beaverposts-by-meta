5d730250599aaf0280f8c246f52b8d51bc180c7d
Didou Schol <didou@badabing.nl>
2020-06-05 14:24:08 +0200

    bugfix when get_postion returns position 0 (first item in array) it is considered as false, and insert gets applied to end instead of after or before index 0

--- beaverposts-by-meta.php
+++ beaverposts-by-meta.php
@@ -10,7 +10,7 @@
  * Plugin Name: Beaver Posts By Meta
  * Plugin URI:  https://www.badabing.nl
  * Description: Add options to the Beaver Posts Module to only show posts that have a certain meta_key value
- * Version:     1.0.0
+ * Version:     1.1.0
  * Author:      Badabingbreda
  * Author URI:  https://www.badabing.nl
  * Text Domain: beaverposts-by-meta
@@ -18,7 +18,7 @@
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  */
 
-define( 'BEAVERPOSTSBYMETA_VERSION' 	, '1.0.0' );
+define( 'BEAVERPOSTSBYMETA_VERSION' 	, '1.1.0' );
 define( 'BEAVERPOSTSBYMETA_DIR'			, plugin_dir_path( __FILE__ ) );
 define( 'BEAVERPOSTSBYMETA_FILE'		, __FILE__ );
 define( 'BEAVERPOSTSBYMETA_URL' 		, plugins_url( '/', __FILE__ ) );
--- inc/insert-between.php
+++ inc/insert-between.php
@@ -1,65 +1,63 @@
 <?php
 
-	if (!function_exists('insert_between') ) {
+if (!function_exists('insert_between') ) {
 
-		/**
-		 * Insert an array with named keys at this position
-		 *
-		 * @param  [type] $array    [description]
-		 * @param  [type] $insert   [description]
-		 * @param  string $col_name [description]
-		 * @param  string $position [description]
-		 * @return [type]           [description]
-		 */
-		function insert_between( $array , $insert , $key_name = 'title' , $position = 'after' ) {
-
-
-			$key_pos = get_position( $array , $key_name );
-
-			// if the key wasn't found, just add it to the end
-			if ( !$key_pos ) return array_merge( $array , $insert );
-
-			switch ( $position ) {
-
-				case "after":
-				    $before = array_slice( $array , 0 , $key_pos + 1 );
-				    $after = array_slice( $array , $key_pos );
-
-				break;
-
-				case "before":
-
-				    $before = array_slice( $array , 0 , $key_pos );
-				    $after = array_slice( $array , $key_pos );
-
-				break;
-
-			}
-
-			return $before + $insert + $after;
-
-		}
+    /**
+     * Insert an array with named keys at this position
+     *
+     * @param  [type] $array    [description]
+     * @param  [type] $insert   [description]
+     * @param  string $col_name [description]
+     * @param  string $position [description]
+     * @return [type]           [description]
+     */
+    function insert_between( $array , $insert , $key_name = 'title' , $position = 'after' ) {
 
 
-	}
+        $key_pos = get_position( $array , $key_name );
 
-	if (!function_exists( 'get_position' ) ) {
-		/**
-		 * get and return the position of a certain key in an array
-		 * @param  [type] $array [description]
-		 * @param  [type] $key   [description]
-		 * @return [type]        [description]
-		 */
-		function get_position( $array , $key ) {
+        // if the key wasn't found, just add it to the end
+        if ( !$key_pos === false ) return array_merge( $array , $insert );
 
-			if ( isset( $array[ $key ] ) || array_key_exists( $array , $key ) ) {
-				return array_search( $key, array_keys( $array ) );
-			}
+        switch ( $position ) {
 
-			return false;
-		}
+            case "after":
+                $before = array_slice( $array , 0 , $key_pos + 1 );
+                $after = array_slice( $array , $key_pos );
+
+            break;
+
+            case "before":
+
+                $before = array_slice( $array , 0 , $key_pos );
+                $after = array_slice( $array , $key_pos );
+
+            break;
+
+        }
+
+        return $before + $insert + $after;
+
+    }
 
 
-	}
+}
+
+if (!function_exists( 'get_position' ) ) {
+    /**
+     * get and return the position of a certain key in an array
+     * @param  [type] $array [description]
+     * @param  [type] $key   [description]
+     * @return [type]        [description]
+     */
+    function get_position( $array , $key ) {
+
+        if ( isset( $array[ $key ] ) || array_key_exists( $key , $array ) ) {
+            return array_search( $key, array_keys( $array ) );
+        }
+
+        return false;
+    }
 
 
+}
